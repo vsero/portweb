e4beb6fdc6386352d5d6c42072e29bedab1d20f2
vsero <schweigen13@gmail.com>
2026-01-10 21:10:20 +1100

    debug

--- TrackingPage/Services/UserContext.cs
+++ TrackingPage/Services/UserContext.cs
@@ -1,4 +1,6 @@
-﻿namespace TrackingPage.Services;
+﻿using System.Text.Json.Serialization;
+
+namespace TrackingPage.Services;
 
 public class UserContext
 {
@@ -37,7 +39,23 @@ public class UserContext
     public long TgUserId { get; set; }
     public string TgUserName { get; set; } = string.Empty;
     public string TgChatId { get; set; } = string.Empty;
-    public class TgInitData { public TgUser User { get; set; } = new(); }
-    public class TgUser { public long Id { get; set; } public string Username { get; set; } = string.Empty; }
+    public class TgInitData
+    {
+        // Telegram присылает "user", JsonSerializer с PropertyNameCaseInsensitive его найдет
+        public TgUser? User { get; set; }
+
+        // Можно добавить другие поля для отладки
+        public string? Hash { get; set; }
+        public string? Signature { get; set; }
+    }
+
+    public class TgUser
+    {
+        public long Id { get; set; }
+        public string? Username { get; set; }
+
+        [JsonPropertyName("first_name")]
+        public string? FirstName { get; set; }
+    }
 
 }
